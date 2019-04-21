5141e4edff932113760bcf3c7457db65021f6c30
ducng99 <tuanduc249.td@gmail.com>
2019-04-21 23:45:04 +1200

    version 1

---
+++ Nightbot/Main.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Nightbot
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(990, 649);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://beta.nightbot.tv/song_requests", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 649);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Nightbot";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Nightbot/Main.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nightbot
+{
+    public partial class Main : Form
+    {
+        bool onPage = false;
+        HtmlDocument doc;
+        
+        public Main()
+        {
+            InitializeComponent();
+            SHDocVw.WebBrowser popupHandle = (SHDocVw.WebBrowser)webBrowser1.ActiveXInstance;
+            popupHandle.NewWindow3 += PopupHandle_NewWindow3;
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void PopupHandle_NewWindow3(ref object ppDisp, ref bool Cancel, uint dwFlags, string bstrUrlContext, string bstrUrl)
+        {
+            Cancel = true;
+            webBrowser1.Navigate(new Uri(bstrUrl));
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Url.AbsoluteUri.Contains("beta.nightbot.tv/song_requests"))
+            {
+                onPage = true;
+                doc = webBrowser1.Document;
+            }
+            else
+            {
+                onPage = false;
+                doc = null;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (onPage)
+                {
+                    try
+                    {
+                        HtmlElementCollection iframes = doc.GetElementsByTagName("iframe");
+                        foreach (HtmlElement player in iframes)
+                        {
+                            if (player.Id.Equals("yt_player") || player.Id.Equals("sc_player"))
+                            {
+                                player.SetAttribute("width", "1");
+                                player.SetAttribute("height", "1");
+                            }
+                        }
+
+                        HtmlElement titleElement = doc.GetElementsByTagName("h4")[0].GetElementsByTagName("strong")[0];
+                        string title = Regex.Split(titleElement.InnerText, " — ")[0];
+                        HtmlElementCollection pElements = doc.GetElementsByTagName("p");
+                        string user = "";
+                        
+                        for (int pI = 0; pI < pElements.Count; pI++)
+                        {
+                            HtmlElementCollection iElements = pElements[pI].GetElementsByTagName("i");
+                            for (int iI = 0; iI < iElements.Count; iI++)
+                            {
+                                if (iElements[iI].GetAttribute("className").Contains("fa-user"))
+                                {
+                                    user = pElements[pI].InnerText;
+                                    break;
+                                }
+                            }
+                        }
+
+                        File.WriteAllText(@"current_song.txt", title + " - Requested by:" + user);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
